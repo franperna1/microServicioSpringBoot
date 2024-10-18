e30d5da34ec21cbbbbe29d595a469501c4ce8e32
Frank <frankigiu15@gmail.com>
2024-10-18 10:11:38 -0300

    inicio del proyecto

---
+++ src/main/java/com/example/microServicioBase/MicroServicioBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.microServicioBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicioBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microServicioBase/MicroServicioBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microServicioBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicioBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
